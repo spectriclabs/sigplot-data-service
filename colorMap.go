d79de5619f9bff0d055cd263f9919827158026b3
Michael Recachinas <mike@recachinas.dev>
2021-10-17 18:59:40 -0400

    Run go fmt on colorMap.go

--- colorMap.go
+++ colorMap.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"math"
 	"log"
+	"math"
 )
 
 type colorPoint struct {
@@ -19,9 +19,9 @@ func makeColorPalette(controlColors []colorPoint, numColors int) []colorPoint {
 	//If first control color is not at 0 then copy color for range
 	lastIndexFilled := 0
 	outColors := make([]colorPoint, numColors)
-	outColors[0].red = math.Round(controlColors[0].red*255.0/100.0)
-	outColors[0].blue = math.Round(controlColors[0].blue*255.0/100.0)
-	outColors[0].green= math.Round(controlColors[0].green*255.0/100.0)
+	outColors[0].red = math.Round(controlColors[0].red * 255.0 / 100.0)
+	outColors[0].blue = math.Round(controlColors[0].blue * 255.0 / 100.0)
+	outColors[0].green = math.Round(controlColors[0].green * 255.0 / 100.0)
 	for i := 1; i < len(controlColors); i++ {
 		//possDiff:=controlColors[i].position-lastPoint.position
 		redDiff := (float64(controlColors[i].red) - float64(lastPoint.red)) * 255.0 / 100
@@ -30,7 +30,7 @@ func makeColorPalette(controlColors []colorPoint, numColors int) []colorPoint {
 		startRange := lastIndexFilled + 1
 		endRange := int(math.Round(float64(controlColors[i].position) * float64(colorsPerPosition)))
 		for j := startRange; j < endRange; j++ {
-			
+
 			percentRange := (float64(j+1) - float64(startRange)) / float64((endRange - startRange))
 			outColors[j].red = (math.Round(percentRange*redDiff + float64(lastPoint.red)*255.0/100))
 			outColors[j].green = (math.Round(percentRange*greenDiff + float64(lastPoint.green)*255.0/100))
@@ -81,34 +81,34 @@ func getColorConrolPoints(colorMap string) []colorPoint {
 		outColors[6] = colorPoint{100, 95, 95, 95}
 	case "calewhite":
 		outColors = make([]colorPoint, 7)
-		outColors[0] =colorPoint{ 0,100,100,100}
-		outColors[1] =colorPoint{ 16.666,0,0,100}
-		outColors[2] =colorPoint{ 33.333,0,100,100}
-		outColors[3] =colorPoint{ 50,0,100,0}
-		outColors[4] =colorPoint{ 66.666,100,100,0}
-		outColors[5] =colorPoint{ 83.333,100,0,0}
-		outColors[6] =colorPoint{ 100,100,0,100}
+		outColors[0] = colorPoint{0, 100, 100, 100}
+		outColors[1] = colorPoint{16.666, 0, 0, 100}
+		outColors[2] = colorPoint{33.333, 0, 100, 100}
+		outColors[3] = colorPoint{50, 0, 100, 0}
+		outColors[4] = colorPoint{66.666, 100, 100, 0}
+		outColors[5] = colorPoint{83.333, 100, 0, 0}
+		outColors[6] = colorPoint{100, 100, 0, 100}
 	case "HotDesat":
 		outColors = make([]colorPoint, 8)
-		outColors[0] =colorPoint{ 0,27.84,27.84,85.88}
-		outColors[1] =colorPoint{ 14.2857,0,0,35.69}
-		outColors[2] =colorPoint{ 28.571,0,100,100}
-		outColors[3] =colorPoint{ 42.857,0,49.8,0}
-		outColors[4] =colorPoint{ 57.14286,100,100,0}
-		outColors[5] =colorPoint{ 71.42857,100,37.65,0}
-		outColors[6] =colorPoint{ 85.7143,41.96,0,0}
-		outColors[7] =colorPoint{ 100,87.84,29.8,29.8}
+		outColors[0] = colorPoint{0, 27.84, 27.84, 85.88}
+		outColors[1] = colorPoint{14.2857, 0, 0, 35.69}
+		outColors[2] = colorPoint{28.571, 0, 100, 100}
+		outColors[3] = colorPoint{42.857, 0, 49.8, 0}
+		outColors[4] = colorPoint{57.14286, 100, 100, 0}
+		outColors[5] = colorPoint{71.42857, 100, 37.65, 0}
+		outColors[6] = colorPoint{85.7143, 41.96, 0, 0}
+		outColors[7] = colorPoint{100, 87.84, 29.8, 29.8}
 	case "Sunset":
 		outColors = make([]colorPoint, 7)
-		outColors[0] =colorPoint{ 0,10,0,23}
-		outColors[1] =colorPoint{ 18,34,0,60}
-		outColors[2] =colorPoint{ 36,58,20,47}
-		outColors[3] =colorPoint{ 55,74,20,28}
-		outColors[4] =colorPoint{ 72,90,43,0}
-		outColors[5] =colorPoint{ 87,100,72,0}
-		outColors[6] =colorPoint{ 100,100,100,76}
+		outColors[0] = colorPoint{0, 10, 0, 23}
+		outColors[1] = colorPoint{18, 34, 0, 60}
+		outColors[2] = colorPoint{36, 58, 20, 47}
+		outColors[3] = colorPoint{55, 74, 20, 28}
+		outColors[4] = colorPoint{72, 90, 43, 0}
+		outColors[5] = colorPoint{87, 100, 72, 0}
+		outColors[6] = colorPoint{100, 100, 100, 76}
 	default:
-		log.Println("Unknown Colormap",colorMap, "using default RampColormap" )
+		log.Println("Unknown Colormap", colorMap, "using default RampColormap")
 		outColors = make([]colorPoint, 7)
 		outColors[0] = colorPoint{0, 0, 0, 15}
 		outColors[1] = colorPoint{10, 0, 0, 50}
